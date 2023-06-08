79add4c3ae2262c71edea30096f63bd1fb4ce7ad
pishexod <dimabukov52@gmail.com>
2023-06-08 16:17:41 +0300

    var2

--- src/Main.java
+++ src/Main.java
@@ -18,10 +18,8 @@ public class Main {
     };
 
     private static final String[] CORRECT_ANSWERS = {"b", "a", "c", "d", "b"};
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("Welcome to the Future Simple Quiz!");
         System.out.print("Enter your full name: ");
         String fullName = scanner.nextLine();
